86c3b9dc57a5346bbb793e2065242158b31ef8eb
diksha1786 <47427213+diksha1786@users.noreply.github.com>
2019-04-28 18:47:10 +0530

    Update Dashboard.php

--- try/Dashboard.php
+++ try/Dashboard.php
@@ -6,7 +6,7 @@ mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 include('header.php');
   $var1 =  $_SESSION['userid'];
 
-	$var2 =	$_SESSION['username']
+	$var2 =	$_SESSION['username'];
 
 
 ?>
